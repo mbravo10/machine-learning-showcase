a6b91933b503b4864d29e3d92a4f95444b894eab
mbravo10 <mario-bravo99@hotmail.com>
2022-04-18 21:01:07 -0600

    Added deployment files

---
+++ deployment/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def my_form():
+    return render_template('my-form.html')
+ 
+@app.route('/data/', methods = ['POST', 'GET'])
+def data():
+    if request.method == 'GET':
+        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+    if request.method == 'POST':
+        form_data = request.form
+        return render_template('data.html',form_data = form_data)
