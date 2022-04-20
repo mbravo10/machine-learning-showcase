3df92abd9626d4fd93a5e5aa0626a1d5a55ac414
William Jessop <willjessop12@gmail.com>
2022-04-20 11:19:21 -0600

    did some more pickling stuff for the website

--- deployment/app.py
+++ deployment/app.py
@@ -1,4 +1,32 @@
+from cmath import pi
 from flask import Flask, request, render_template
+import string
+import pickle
+
+def remove_stop_words(comment):
+    eng_stops = pickle.load(open('./pickles/eng_stops.pkl', 'rb'))
+    new_comment = []
+    for word in comment.split():
+        if word not in eng_stops:
+            new_comment.append(word)
+        
+    return " ".join(new_comment)
+
+def stem_and_lem(comment):
+    stemmer = pickle.load(open('./pickles/stemmer.pkl', 'rb'))
+    lemmatiser = pickle.load(open('./pickles/lemmatiser.pkl', 'rb'))
+    new_comment = []
+    for word in comment.split():
+        new_comment.append(stemmer.stem(lemmatiser.lemmatize(word, pos='v')))
+    return " ".join(new_comment)
+
+def vectorize(comment):
+    hasher = pickle.load(open('./pickles/hash_vect.pkl', 'rb'))
+    return hasher.transform(comment)
+
+def get_pred(comment):
+    best_sgd = pickle.load(open('./pickles/trained_sgd.pkl', 'rb'))
+    return best_sgd.predict(comment)
 
 app = Flask(__name__)
 
@@ -12,4 +40,17 @@ def data():
         return f"The URL /data is accessed directly. Try going to '/form' to submit form"
     if request.method == 'POST':
         form_data = request.form
+        print(form_data['InputVal'])
+
+        data = remove_stop_words(form_data['InputVal'])
+        data = data.translate(str.maketrans('', '', string.punctuation + "1234567890")).lower()
+        data = stem_and_lem(data)
+        data = vectorize([data])
+
+        prediction = get_pred(data)
+
+        # 1 is a toxic
+        # 0 is non-toxic
+        print(prediction)
+
         return render_template('data.html',form_data = form_data)
